44469007fd1b9ba32c6c2044a569b715f333bbdc
martinez-mar <mtzmarthaa23>
2026-06-02 13:42:28 -0600

    To do list

---
+++ To Do List/script.js
@@ -0,0 +1,64 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Cargar tareas guardadas
+renderTasks();
+
+addBtn.addEventListener("click", addTask);
+
+// Permitir Enter
+taskInput.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        addTask();
+    }
+});
+
+function addTask(){
+
+    const taskText = taskInput.value.trim();
+
+    if(taskText === ""){
+        return;
+    }
+
+    tasks.push(taskText);
+
+    saveTasks();
+    renderTasks();
+
+    taskInput.value = "";
+}
+
+function renderTasks(){
+
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            <span>${task}</span>
+            <button class="delete-btn" onclick="deleteTask(${index})">
+                x
+            </button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+function deleteTask(index){
+
+    tasks.splice(index, 1);
+
+    saveTasks();
+    renderTasks();
+}
+
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
